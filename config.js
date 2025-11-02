dd64a198ed96b615d735e2769e4555a8911b9138
doantuyen15 <doantuyen15@gmail.com>
2025-11-02 14:11:45 +0700

    Deploy

---
+++ config.js
@@ -0,0 +1,15 @@
+// Runtime configuration loaded before the React bundle.
+// Update values here after deployment without rebuilding.
+window.__APP_CONFIG__ = {
+  ENV: 'UAT',
+  fireBaseConfig: {
+    apiKey: 'AIzaSyByvZ4ShMb7ROh7mh9xHByn58bTZqQuDKQ',
+    authDomain: 'weaver-crm.firebaseapp.com',
+    databaseURL: 'https://weaver-crm-default-rtdb.asia-southeast1.firebasedatabase.app',
+    projectId: 'weaver-crm',
+    storageBucket: 'weaver-crm.appspot.com',
+    messagingSenderId: '707203151294',
+    appId: '1:707203151294:web:c512db715a964295fa75ef',
+    measurementId: 'G-80YDQLRC55',
+  },
+};
